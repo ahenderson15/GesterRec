1f5b88144053f382150e9daac209b9ffd11d7563
ahenderson15 <aphenderson15@gmail.com>
2020-10-15 09:43:53 -0400

    Initial Commit

---
+++ GesterRec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GesterRec
+//
+//  Created by  on 10/15/20.
+//  Copyright © 2020 andrewsApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
